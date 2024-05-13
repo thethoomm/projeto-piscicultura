6041499dd3e4aa1799f6ff45fc7f24777c883a26
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-13 19:04:10 -0300

    create thingspeak constants

---
+++ src/constants/thingspeak.ts
@@ -0,0 +1,5 @@
+export const CANAL_LIMITE_SENSORES_1 = '23YC8RS6DRHAJX93'
+export const CANAL_LIMITE_SENSORES_2 = 'OKPJYMR8BY4DS76U'
+export const CANAL_MANUAL_AUTOMATICO = 'GYEY7LPTK3JRKEP1'
+
+export const THINGSPEAK_URL = 'https://api.thingspeak.com/'
