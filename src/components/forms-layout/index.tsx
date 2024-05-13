48d46e182b2cde110092913a344c50bb297b3edb
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-13 19:37:24 -0300

    create forms layout

---
+++ src/components/forms-layout/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+type FormsLayoutProps = {
+  title: string
+  description: string
+  children: React.ReactNode
+}
+
+export function FormsLayout(props: FormsLayoutProps) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.description}>
+          {props.description}
+        </Text>
+      </View>
+      <View>
+        {props.children}
+      </View>
+    </View>
+  );
+}
---
+++ src/components/forms-layout/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import { theme } from "@/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+  header: {
+    width: '100%',
+    height: 200,
+    padding: 20,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    backgroundColor: theme.colors.primary,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: theme.colors.background
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: theme.colors.background
+  }
+})
