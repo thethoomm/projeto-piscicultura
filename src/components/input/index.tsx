017a8b9fce79bce704fd270e7a6e4dde8321cd36
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-13 22:15:33 -0300

    create input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,7 @@
+import { TextInput, TextInputProps } from "react-native";
+import { styles } from "./style";
+export function Input({...rest}: TextInputProps) {
+  return (
+    <TextInput style={styles.input} {...rest}/>
+  )
+}
---
+++ src/components/input/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { theme } from "@/theme";
+
+export const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+    height: 48,
+    paddingHorizontal: 12,
+    borderWidth: 1.7,
+    borderColor: theme.colors.primary,
+    borderRadius: 6,
+    fontSize: 16,
+  },
+})
