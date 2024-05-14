9e7f3f55572961bc0257911e9694a45c1713533d
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-14 11:32:02 -0300

    create render input list

---
+++ src/components/render-input-list/index.tsx
@@ -0,0 +1,32 @@
+import { Control, FieldValues } from "react-hook-form";
+import { sensores } from "@/utils/sensores";
+import { Text, View } from "react-native";
+import { Input } from "@/components/input";
+import { styles } from "./style";
+
+type RenderInputListProps = {
+  control: Control<FieldValues>;
+};
+
+export function RenderInputList({ control }: RenderInputListProps) {
+  
+  return sensores.map((sensor) => (
+    <View key={sensor.name} style={styles.formGroup}>
+      <Text style={styles.label}>{sensor.name.toUpperCase()}</Text>
+      <View style={styles.inputGroup}>
+        <Input
+          control={control}
+          name={sensor.max}
+          placeholder={sensor.max}
+          keyboardType="numeric"
+        />
+        <Input
+          control={control}
+          name={sensor.min}
+          placeholder={sensor.min}
+          keyboardType="numeric"
+        />
+      </View>
+    </View>
+  ));
+}
---
+++ src/components/render-input-list/style.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  formGroup: {
+    flex: 1,
+    marginBottom: 16,
+    flexDirection: 'column',
+    gap: 8,
+  },
+  inputGroup: {
+    flexDirection: 'column',
+    gap: 12,
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
+})
