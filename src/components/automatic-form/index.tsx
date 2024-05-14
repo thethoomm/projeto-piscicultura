1d2d5c3418d2c55a042c4c11d51bc4f4b7b021af
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-14 12:32:19 -0300

    add input and button to form

--- src/components/automatic-form/index.tsx
+++ src/components/automatic-form/index.tsx
@@ -1,9 +1,26 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, View, Switch } from "react-native";
+import { RenderSwitchList } from "../render-switch-list";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/button";
 
 export function AutomaticForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
+
+  const handleSendToThingspeak = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <ScrollView>
-      <Text>Formulario do modo automático</Text>
+      <RenderSwitchList control={control}/>
+      <Button
+        text="Enviar"
+        onPress={handleSubmit(handleSendToThingspeak)}
+      />
     </ScrollView>
   );
 }
