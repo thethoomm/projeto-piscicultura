13c1030860d6656dc8f7126c309a1a46ab291b49
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-14 12:05:08 -0300

    create automatic form

---
+++ src/components/automatic-form/index.tsx
@@ -0,0 +1,9 @@
+import { ScrollView, Text, View } from "react-native";
+
+export function AutomaticForm() {
+  return (
+    <ScrollView>
+      <Text>Formulario do modo automático</Text>
+    </ScrollView>
+  );
+}
