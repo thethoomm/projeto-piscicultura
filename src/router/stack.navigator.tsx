706033ff00b9a9c57574b317503476f63b4d812e
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-14 18:14:06 -0300

    add assets

--- src/router/stack.navigator.tsx
+++ src/router/stack.navigator.tsx
@@ -1,27 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import LoginScreen from "../screens/login";
 import { TabNavigator } from "./tab.navigator";
 import UploadingScreen from "../screens/uploading";
-import { NavigatorScreenParams } from "@react-navigation/native";
 
 export type RootStackParams = {
-  login: undefined
-  tabs: undefined
-  uploading: undefined
-}
+  login: undefined;
+  tabs: undefined;
+  uploading: undefined;
+};
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export function StackNavigator() {
   return (
-    <Stack.Navigator initialRouteName="tabs" screenOptions={{
-      headerShown: false
-    }}>
-      <Stack.Screen name="login" component={LoginScreen}/>
-      <Stack.Screen name="tabs" component={TabNavigator}/>
-      <Stack.Screen name="uploading" component={UploadingScreen}/>
+    <Stack.Navigator
+      initialRouteName="tabs"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="tabs" component={TabNavigator} />
+      <Stack.Screen name="uploading" component={UploadingScreen} />
     </Stack.Navigator>
-  )
+  );
 }
