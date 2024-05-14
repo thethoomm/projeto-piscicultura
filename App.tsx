63470996781c53ff0a4483ffd77689140f94b1a6
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2024-05-14 16:56:59 -0300

    add useLoading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/router/stack.navigator';
+import { NavigationContainer } from "@react-navigation/native";
+import { StackNavigator } from "./src/router/stack.navigator";
+import { LoadingProvider } from "./src/contexts/useLoading";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <LoadingProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </LoadingProvider>
   );
 }
